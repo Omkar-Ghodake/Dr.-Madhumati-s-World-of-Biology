459afdd1b4cfe7f8e72d4dbae584d6de8159dd11
test-bot-69 <testbot.1831@gmail.com>
2024-04-03 13:38:51 +0530

    Changed color of navbar toggler

--- components/navbar/NavbarToggler.tsx
+++ components/navbar/NavbarToggler.tsx
@@ -1,5 +1,8 @@
+'use client'
+
 import React from 'react'
 import { Button } from '../ui/button'
+import { useTheme } from '@/context/ThemeState'
 
 const NavbarToggler = ({
   isNavbarOpen,
@@ -8,28 +11,30 @@ const NavbarToggler = ({
   isNavbarOpen: boolean
   toggleNavbar: () => void
 }) => {
+  const { theme } = useTheme()
+
   return (
     <Button
       variant={'outline'}
       size={'icon'}
-      className={`flex flex-col justify-evenly  items-center z-50`}
+      className={`flex flex-col justify-evenly items-center z-50 border-primary/35`}
       onClick={toggleNavbar}
     >
       <div
-        className={`w-[22px] h-[1.5px] bg-black ${
+        className={`w-[22px] h-[1.5px] bg-primary ${
           isNavbarOpen &&
           'rotate-[46.5deg] w-[25px] h-[2px] translate-y-[6.29px] translate-x-[1px]'
         } duration-150`}
       ></div>
 
       <div
-        className={`w-[22px] h-[1.5px] bg-black ${
+        className={`w-[22px] h-[1.5px] bg-primary ${
           isNavbarOpen && 'hidden'
         } duration-150`}
       ></div>
 
       <div
-        className={`w-[22px] h-[1.5px] bg-black ${
+        className={`w-[22px] h-[1.5px] bg-primary ${
           isNavbarOpen &&
           '-rotate-[46.5deg] w-[25px] h-[2px] -translate-y-[6.29px] translate-x-[1px]'
         } duration-150`}
