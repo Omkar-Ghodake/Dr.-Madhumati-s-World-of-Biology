3cbd6f2ffa2eb3a179bee2655013469d05b562e6
test-bot-69 <testbot.1831@gmail.com>
2024-04-03 22:26:54 +0530

    Added z-index to navbar

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -32,7 +32,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className='w-[100vw] h-[10vh] blur-b border-b border-primary/80 shadow-lg md:hidden flex justify-between items-center px-2'>
+      <div className='w-[100vw] h-[10vh] blur-b border-b border-primary/80 shadow-lg md:hidden flex justify-between items-center px-2 z-50'>
         <Link href={'/'} className='navbar-brand mt-0-imp z-50'>
           <NavbarBrand width={200} height={40} />
         </Link>
@@ -49,9 +49,9 @@ const Navbar = () => {
       {/* // TODO: Alignment of the navbar toggler button */}
 
       <nav
-        className={`absolute left-0 md:static w-full h-fit md:h-[12vh] flex flex-col md:flex-row md:justify-center items-center space-y-5 md:space-y-0 px-1 py-10 md:py-0 border-b nav-blur-bg ${
+        className={`absolute left-0 md:static w-full h-fit md:h-[12vh] flex flex-col md:flex-row md:justify-center items-center space-y-5 md:space-y-0 px-1 py-5 md:py-0 border-b nav-blur-bg ${
           isNavbarOpen ? '-translate-y-0' : '-translate-y-[120vh]'
-        } md:-translate-y-0 duration-300`}
+        } md:-translate-y-0 duration-300 z-40`}
         ref={navRef}
       >
         <div className='left-navlinks md:w-[40vw] flex flex-col md:flex-row items-center justify-center text-lg md:mt-0'>
