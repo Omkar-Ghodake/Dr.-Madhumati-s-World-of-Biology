abc62301528f08288b1e8a784371a12ca166936a
test-bot-69 <testbot.1831@gmail.com>
2024-04-09 08:51:09 +0530

    updated home font sizes

--- app/page.tsx
+++ app/page.tsx
@@ -9,25 +9,20 @@ export default function Home() {
   return (
     <MaxWidthWrapper className='flex flex-col md:flex-row justify-between items-center'>
       <div className='left w-full md:w-3/5 flex flex-col justify-center space-y-1 md:space-y-5 order-2 flex-1 md:order-1'>
-        <b className='text-4xl md:text-5xl text-slate-600'>
+        <b className='text-3xl md:text-5xl text-slate-600'>
           <i>WELCOME TO</i>
         </b>
 
         <strong className={`${montserrat.className} leading-tight`}>
           <span className=''>
-            <span className='text-3xl md:text-5xl'>Dr. </span>
-            <br />
-            <span className='text-[2.7rem] md:text-[4.8rem]'>
-              <span className='text-primary'>Madhu</span>
-              <span>mati&apos;s</span>
-            </span>
+            <span className='text-4xl md:text-5xl'>Dr. </span>
+            <span className='text-4xl md:text-5xl'>Madhutmati&apos;s</span>
           </span>
 
           <br />
 
-          <span className='text-3xl md:text-5xl'>
-            <span className='text-primary'>World of </span>
-            <span>Biology</span>
+          <span className='text-5xl md:text-[4.8rem] text-primary'>
+            World of Biology
           </span>
         </strong>
 
