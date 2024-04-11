1ed3d2cd23d11704b7c580316a1e273d4a21850e
test-bot-69 <testbot.1831@gmail.com>
2024-04-11 10:55:37 +0530

    Created weprovide menu-0

--- components/weProvide/WeProvideMenu.tsx
+++ components/weProvide/WeProvideMenu.tsx
@@ -33,7 +33,7 @@ const WeProvideMenu = ({
           '-translate-x-[180vw] md:-translate-x-[150vw]'
         }`}
       >
-        <div className='menu-0 h-full md:p-5 w-[90vw] md:w-[75vw] flex flex-col md:flex-row justify-between space-y-5 md:space-x-5'>
+        <div className='menu-0 h-full md:p-5 w-[90vw] md:w-[75vw] flex flex-col md:flex-row justify-between space-y-5 md:space-y-0 md:space-x-5'>
           {JR_COLLEGE_BATCH_INFO.map((item) => (
             <BatchInfo
               key={item.cardTitle}
@@ -46,14 +46,14 @@ const WeProvideMenu = ({
           ))}
         </div>
 
-        <div className='menu-1 h-full md:p-5  w-[90vw] md:w-[75vw]'>
+        <div className='menu-1 h-full p-5  w-[90vw] md:w-[75vw]'>
           1 - Lorem ipsum dolor sit amet, consectetur adipisicing elit.
           Asperiores officia beatae qui, consectetur laborum recusandae corporis
           unde mollitia? Fugiat, cupiditate! Voluptas nihil nesciunt laudantium
           ullam nam quo repudiandae maxime possimus.
         </div>
 
-        <div className='menu-2 h-full md:p-5  w-[90vw] md:w-[75vw]'>
+        <div className='menu-2 h-full p-5  w-[90vw] md:w-[75vw]'>
           2 - Lorem ipsum dolor sit amet, consectetur adipisicing elit.
           Asperiores officia beatae qui, consectetur laborum recusandae corporis
           unde mollitia? Fugiat, cupiditate! Voluptas nihil nesciunt laudantium
