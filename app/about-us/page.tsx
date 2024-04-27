84bc331e3874c87ba367fa43ec0bdb6f0d86ba03
test-bot-69 <testbot.1831@gmail.com>
2024-04-27 21:55:39 +0530

    made about-us page responsive

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -4,15 +4,17 @@ import React from 'react'
 
 const AboutUs = () => {
   return (
-    <MaxWidthWrapper className='flex justify-between space-x-10 pt-24'>
-      <div className=''>
-        <Image src={'/medical_expert.png'} alt='' width={500} height={500} />
+    <MaxWidthWrapper className='flex flex-col md:flex-row justify-between items-center md:space-x-10 space-y-5 md:space-y-0 pt-10 md:pt-24 px-0 md:px-5'>
+      <div className='relative h-[25vh] md:h-[50vh] w-[25vh] md:w-[50vh]'>
+        <Image src={'/medical_expert.png'} alt='' fill />
       </div>
 
-      <div className='flex-1 h-full p-5'>
-        <h3 className='text-4xl'>Dr. Madhumati Maskar</h3>
-        <h1 className='text-4xl font-semibold drop-shadow-lg'>DIRECTOR</h1>
-        <h2 className='text-5xl font-semibold drop-shadow-lg text-primary'>
+      <div className='flex-1 h-full md:p-5'>
+        <h3 className='text-3xl md:text-4xl'>Dr. Madhumati Maskar</h3>
+        <h1 className='text-3xl md:text-4xl font-semibold drop-shadow-lg'>
+          DIRECTOR
+        </h1>
+        <h2 className='text-4xl md:text-5xl font-semibold drop-shadow-lg text-primary'>
           Dr Madhumati&apos;s World of Biology
         </h2>
       </div>
