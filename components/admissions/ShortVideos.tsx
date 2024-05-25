f4ca1e21160ff82ca5431d00e451879ae68441f4
Sachin P. <spaithane9172@gmail.com>
2024-05-25 18:34:36 +0530

    shortVideo nevigation changes

--- components/admissions/ShortVideos.tsx
+++ components/admissions/ShortVideos.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 import { motion } from "framer-motion";
+import { useState } from "react";
 
 const ShortVideos = () => {
+  const [isVideoActive, setIsVideoActive] = useState(1);
   return (
     <div className="md:pt-5">
       <div className="w-full overflow-hidden overflow-x-scroll md:overflow-x-hidden scrollbar-sm p-2 md:scale-[1.3]">
@@ -26,7 +28,7 @@ const ShortVideos = () => {
           <motion.div
             initial={{ scale: 0 }}
             whileInView={{ scale: 1 }}
-            transition={{ delay: 0.1 }}
+            transition={{ delay: 0.2 }}
             className="relative w-[20rem] md:w-fit h-[30rem] md:h-[50vh] rounded-xl select-none"
             id="video2"
           >
@@ -42,7 +44,7 @@ const ShortVideos = () => {
           <motion.div
             initial={{ scale: 0 }}
             whileInView={{ scale: 1 }}
-            transition={{ delay: 0.1 }}
+            transition={{ delay: 0.3 }}
             className="relative w-[20rem] md:w-fit h-[30rem] md:h-[50vh] rounded-xl select-none"
             id="video3"
           >
@@ -58,7 +60,7 @@ const ShortVideos = () => {
           <motion.div
             initial={{ scale: 0 }}
             whileInView={{ scale: 1 }}
-            transition={{ delay: 0.1 }}
+            transition={{ delay: 0.4 }}
             className="relative w-[20rem] md:w-fit h-[30rem] md:h-[50vh] rounded-xl select-none"
             id="video4"
           >
@@ -73,25 +75,53 @@ const ShortVideos = () => {
           </motion.div>
         </div>
       </div>
-      <div className="flex justify-center items-center">
-        <a href="#video1">
+      <div className="flex justify-center items-center md:hidden">
+        <a
+          href="#video1"
+          onClick={() => {
+            setIsVideoActive(1);
+          }}
+        >
           <div
-            className={`bg-primary w-[1rem] h-[1rem] rounded-full mx-2`}
+            className={`bg-primary ${
+              isVideoActive == 1 ? "w-[2rem]" : "w-[1rem]"
+            } h-[1rem] rounded-full mx-2`}
           ></div>
         </a>
-        <a href="#video2">
+        <a
+          href="#video2"
+          onClick={() => {
+            setIsVideoActive(2);
+          }}
+        >
           <div
-            className={`bg-primary w-[1rem] h-[1rem] rounded-full mx-2`}
+            className={`bg-primary ${
+              isVideoActive == 2 ? "w-[2rem]" : "w-[1rem]"
+            } h-[1rem] rounded-full mx-2`}
           ></div>
         </a>
-        <a href="#video3">
+        <a
+          href="#video3"
+          onClick={() => {
+            setIsVideoActive(3);
+          }}
+        >
           <div
-            className={`bg-primary w-[1rem] h-[1rem] rounded-full mx-2`}
+            className={`bg-primary ${
+              isVideoActive == 3 ? "w-[2rem]" : "w-[1rem]"
+            } h-[1rem] rounded-full mx-2`}
           ></div>
         </a>
-        <a href="#video4">
+        <a
+          href="#video4"
+          onClick={() => {
+            setIsVideoActive(4);
+          }}
+        >
           <div
-            className={`bg-primary w-[1rem] h-[1rem] rounded-full mx-2`}
+            className={`bg-primary ${
+              isVideoActive == 4 ? "w-[2rem]" : "w-[1rem]"
+            } h-[1rem] rounded-full mx-2`}
           ></div>
         </a>
       </div>
