7616623908bbbec20c7138da301ff2fa4cdcce78
Sachin P. <spaithane9172@gmail.com>
2024-05-25 15:55:04 +0530

    admission page work

--- components/admissions/AdmissionsHead.tsx
+++ components/admissions/AdmissionsHead.tsx
@@ -1,27 +1,24 @@
 import React from "react";
 import Image from "next/image";
-import medicalExpert from "@/public/medical_expert.png";
+import medicalStudent from "@/public/ads/home_ad_banner.jpg";
 
 const AdmissionsHead = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-between items-center mb-10 md:mb-20">
-      <div className="relative h-[40vh] md:h-[60vh] w-[30vh] md:w-[50vh] mb-10 md:mb-0">
+    <div className="flex flex-col md:flex-row justify-between items-center mb-10 md:mb-20 md:pt-10">
+      <div className="relative h-[40vh] md:h-[50vh] w-full md:w-1/2">
         <Image
-          src={medicalExpert}
+          src={medicalStudent}
           alt=""
           fill
           placeholder="blur"
-          className="object-contain drop-shadow-2xl"
+          className="object-contain p-2"
         />
       </div>
-      <div className="relative h-[40vh] md:h-[60vh] w-[30vh] md:w-[50vh] mb-10 md:mb-0">
-        <Image
-          src={medicalExpert}
-          alt=""
-          fill
-          placeholder="blur"
-          className="object-contain drop-shadow-2xl"
-        />
+      <div className="relative flex justify-center items-center h-[40vh] md:h-[50vh] w-full md:w-1/2">
+        <video controls autoPlay={true} className="w-full h-full p-2">
+          <source src={"/shortVideos/mainVideo.mp4"} />
+          Your browser does not support the video tag...
+        </video>
       </div>
     </div>
   );
--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,12 @@ const nextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: 'https',
-        hostname: 'avataaars.io',
+        protocol: "https",
+        hostname: "avataaars.io",
       },
     ],
     dangerouslyAllowSVG: true,
   },
-}
+};
 
-export default nextConfig
+export default nextConfig;
